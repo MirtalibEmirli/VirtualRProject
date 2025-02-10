d9a2dd74bdd37540974de32e7e647eb090b22ec2
Amirl_kl34@itstep.edu.az <Amirl_kl34@itstep.edu.az>
2025-02-10 15:34:58 +0400

    day 3 with React pushing myself to dicipline

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import HeroSection from './components/HeroSection'
+import FeatureSession from './components/FeatureSession'
 const App = () => {
   return (
    <>
    <Navbar></Navbar>
    <HeroSection></HeroSection>
+   <FeatureSession></FeatureSession>
    </>
   )
 }
